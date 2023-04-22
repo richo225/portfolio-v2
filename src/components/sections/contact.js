6ba6266f0bdc7f2034e8fe7935fda6f69df806c4
Richard Bates <rich.bates@protonmail.com>
2023-04-22 15:06:06 +0800

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I'm currently open to new opportunities, preferably remote or based in London and Singapore.
+        Whether you're looking for an engineer or have a question, drop me an emal!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
