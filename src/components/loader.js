e4762112653cdda0b3042bf80338a47cb729eacb
Richard Bates <rich.bates@protonmail.com>
2023-04-24 23:08:10 +0800

    Adds loader icon and anime animations

--- src/components/loader.js
+++ src/components/loader.js
@@ -41,38 +41,51 @@ const Loader = ({ finishLoading }) => {
 
   const animate = () => {
     const loader = anime.timeline({
+      easing: 'easeInOutSine',
       complete: () => finishLoading(),
     });
 
     loader
-      .add({
-        targets: '#logo path',
-        delay: 300,
-        duration: 1500,
-        easing: 'easeInOutQuart',
-        strokeDashoffset: [anime.setDashoffset, 0],
-      })
-      .add({
-        targets: '#logo #B',
-        duration: 700,
-        easing: 'easeInOutQuart',
-        opacity: 1,
-      })
-      .add({
-        targets: '#logo',
-        delay: 500,
-        duration: 300,
-        easing: 'easeInOutQuart',
-        opacity: 0,
-        scale: 0.1,
-      })
-      .add({
-        targets: '.loader',
-        duration: 200,
-        easing: 'easeInOutQuart',
-        opacity: 0,
-        zIndex: -1,
-      });
+      .add({ targets: '.sixth-rect1', translateY: 54, duration: 500 })
+      .add({ targets: '.sixth-rect2', translateX: -54, duration: 500 }, 0)
+      .add({ targets: '.sixth-rect3', translateY: -54, duration: 500 }, 0)
+      .add({ targets: '.sixth-rect4', translateX: 54, duration: 500, endDelay: 200 }, 0);
+
+    loader.add({
+      targets: '#sixthSVG',
+      duration: 500,
+      rotate: 135,
+      endDelay: 200,
+    });
+
+    loader
+      .add({ targets: '.sixth-rect2', translateX: 0, duration: 500 })
+      .add({ targets: '.sixth-rect4', translateX: 0, duration: 500, endDelay: 200 }, '-=500');
+
+    loader
+      .add({ targets: '.sixth-rect1', translateY: 0, duration: 500 })
+      .add({ targets: '.sixth-rect4', translateX: 54, duration: 500 }, '-=500');
+
+    loader
+      .add({ targets: '.sixth-rect3', translateY: 0, duration: 500 }, '-=500')
+      .add({ targets: '.sixth-rect2', translateX: -54, duration: 500, endDelay: 200 }, '-=500');
+
+    loader
+      .add({ targets: '.sixth-rect1', translateY: 54, duration: 500 })
+      .add({ targets: '.sixth-rect3', translateY: -54, duration: 500, endDelay: 200 }, '-=500');
+
+    loader.add({
+      targets: '#sixthSVG',
+      duration: 500,
+      rotate: 270,
+      endDelay: 200,
+    });
+
+    loader
+      .add({ targets: '.sixth-rect1', translateY: 0, duration: 500 })
+      .add({ targets: '.sixth-rect2', translateX: 0, duration: 500 }, '-=500')
+      .add({ targets: '.sixth-rect3', translateY: 0, duration: 500 }, '-=500')
+      .add({ targets: '.sixth-rect4', translateX: 0, duration: 500, endDelay: 200 }, '-=500');
   };
 
   useEffect(() => {
