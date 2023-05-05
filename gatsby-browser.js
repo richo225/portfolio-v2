4eb5fb0c9f4b3ebbe71741c5b2b1213efd74e309
Richard Bates <rich.bates@protonmail.com>
2023-05-05 22:28:53 +0100

    Adds shell prompts and linenumber highlighting to prism
    Update PrismStyles.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,5 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+require('prismjs/plugins/line-numbers/prism-line-numbers.css');
+require('prismjs/plugins/command-line/prism-command-line.css');
